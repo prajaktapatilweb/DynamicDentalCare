7fe6b86b221a5ffc11e50c9e709fa2f86242ab30
prajaktapatilweb <prajaktaweb@gmail.com>
2024-05-06 08:50:20 +0530

    upto service section1

--- components/home/popular-course.data.js
+++ components/home/popular-course.data.js
@@ -4,35 +4,35 @@ export const data = [
 
     {
         id: 1,
-        cover: '/images/services/rootcanal4.jpg',
+        cover: '/images/services/rootcanal5.jpg',
         title: 'Root Canal',
         price: <span>Find relief from pain and preserve your natural tooth with our expert <Link href='https://www.dentavenue.in/services/root-canals-in-chembur' sx={{ color: '#551a8b' }}> root canal treatments in Chembur.</Link></span>
 
     },
     {
         id: 2,
-        cover: '/images/services/teethimplant2.jpg',
+        cover: '/images/services/teethimplant3.jpg',
         title: 'Dental Implant',
         price: <span>Restore your smile with confidence through our advanced <Link href="https://www.dentavenue.in/services/dental-implants-in-chembur" sx={{ color: '#551a8b' }}> dental implant solutions </Link> near you.</span>,
 
     },
     {
         id: 3,
-        cover: '/images/services/fullconstr1.jpg',
+        cover: '/images/services/fullconstr2.jpg',
         title: 'Full Mouth Reconstruction',
         price: "Regain optimal oral health and function with our comprehensive full mouth reconstruction treatments in Chembur.",
 
     },
     {
         id: 4,
-        cover: '/images/services/braces1.jpg',
+        cover: '/images/services/braces2.jpg',
         title: 'Braces & Aligners',
         price: <span>Achieve a straighter smile and improved bite with our customized <Link href='https://www.dentavenue.in/services/teeth-braces-in-chembur' sx={{ color: '#551a8b' }}> braces and aligner options</Link> near you.</span>,
 
     },
     {
         id: 5,
-        cover: '/images/services/bridges.jpg',
+        cover: '/images/services/bridges1.jpg',
         title: 'Crown & Bridges',
 
         price: <span>Regain the strength, function, and aesthetics of your teeth with our high-quality <Link href='https://www.dentavenue.in/services/crown-and-bridges-treatment-in-chembur' sx={{ color: '#551a8b' }}> crown and bridge treatments in Chembur</Link>.</span>,
@@ -40,14 +40,14 @@ export const data = [
     },
     {
         id: 6,
-        cover: '/images/services/teethwhite1.jpg',
+        cover: '/images/services/teethwhite2.jpg',
         title: 'Teeth Whitening',
         price: <span>Enhance your smile's brilliance and beauty with our effective <Link href='https://www.dentavenue.in/services/teeth-whitening-in-chembur' sx={{ color: '#551a8b' }}> teeth whitening solutions</Link> near you.</span>,
 
     },
     {
         id: 7,
-        cover: '/images/services/dentistry1.jpg',
+        cover: '/images/services/dentistry2.jpg',
         title: 'Pediatric Dentistry',
 
         price: <span>Ensure your child's dental health with our gentle and specialized <Link href='https://www.dentavenue.in/services/kids-dentist-in-chembur' sx={{ color: '#551a8b' }}> pediatric dental care in Chembur</Link>.</span>,
@@ -57,14 +57,14 @@ export const data = [
 
     {
         id: 8,
-        cover: '/images/services/veeners.jpg',
+        cover: '/images/services/veeners1.jpg',
         title: 'Dental Veneers',
         price: 'Transform your smile with dental veneers, the perfect solution for a stunning and confident appearance near you.',
 
     },
     {
         id: 9,
-        cover: '/images/services/wisdomteeth.jpg',
+        cover: '/images/services/wisdomteeth1.jpg',
         title: 'Wisdom Teeth Extraction',
         price: <span>Trust our experienced team for safe and comfortable <Link href='https://www.dentavenue.in/services/tooth-extraction-in-chembur' sx={{ color: '#551a8b' }}>wisdom teeth extraction</Link> or any other tooth extraction needs in Chembur.</span>,
 
